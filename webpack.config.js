0755d53dee639f04ac69ed7385519352c5703562
Alexander Widua <a.widua@gmail.com>
2021-10-19 15:04:03 +0200

    add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = () => {
 		plugins: [
 			new HtmlWebpackPlugin({
 				template: path.resolve(__dirname, 'public/index.html'),
-				favicon: 'public/favicon.png'
+				favicon: 'public/favicon.ico'
 			}),
 			new Dotenv()
 		],
